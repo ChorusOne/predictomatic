c4bcb0371936489f47b7125b2ab4a969529ca77e
Ruud van Asseldonk <ruud@chorus.one>
2025-10-16 17:10:34 +0200

    Record the formulas for the LMSR AMM trades
    I spent the last few hours trying to make sense of it. Claude and
    ChatGPT were utterly unhelpful, but pen and paper and just plain algebra
    got me there. In the end the equations are so simple and elegant!

--- src/trade.js
+++ src/trade.js
@@ -1,3 +1,33 @@
+function getProbability(balance) {
+    const ps = balance.map(lk => Math.exp(-lk));
+    const sum = ps.reduce((acc, x) => acc + x, 0.0);
+    // For binary questions, outcome 0 is the one we are interested in.
+    return ps[0] / sum;
+}
+
+// Return the delta in pool shares to bring the implied probability to `p`.
+function getTrade(p) {
+    // Comput the current value of the invariant. Whatever we swap, the
+    // invariant must remain the same.
+    const ps = systemBalance.map(lk => Math.exp(-lk));
+    const invariant = ps.reduce((acc, x) => acc + x, 0.0);
+
+    // const targetDiff = Math.log(p / (1.0 - p));
+    // const balanceYesNew = -Math.log(invariant / (1 + Math.exp(-targetDiff)));
+    // const balanceNew = [balanceYesNew, balanceYesNew + targetDiff];
+
+    const newYes = -Math.log(p * invariant);
+    const newNo = -Math.log(invariant - Math.exp(-newYes));
+    // Equivalent:
+    const newNo2 = -Math.log(invariant - p * invariant);
+    // Equivalent:
+    const newNo3 = -Math.log(invariant * (1.0 - p));
+
+    const p2 = getProbability([newYes, newNo3]);
+
+    console.log(p, newYes, newNo, newNo2, newNo3, p2);
+}
+
 function initializeSlider() {
     const widget = document.getElementById("trade-widget");
     const knob = widget.getElementsByClassName("knob")[0];
@@ -28,11 +58,12 @@ function initializeSlider() {
     const positionSlider = (p) => {
         setPos(knob, p);
         setPercentage(tUser, pUser, p);
+        getTrade(p);
     };
     const onDragMove = (event) => {
         const dx = event.clientX - startX;
         const rx = (knobRect.left + 0.5 * knobRect.width - hrRect.left + dx) / hrRect.width;
-        const rxClamp = Math.max(0.0, Math.min(1.0, rx));
+        const rxClamp = Math.max(0.001, Math.min(0.999, rx));
         positionSlider(rxClamp);
     };
     const onDragEnd = (event) => {
@@ -56,8 +87,10 @@ function initializeSlider() {
     knob.addEventListener("mousedown", onDragStart);
     knob.addEventListener("touchstart", onDragStart);
 
-    positionSlider(0.8);
-    setPercentage(tMarket, pMarket, 0.8);
+    const p = getProbability(systemBalance);
+    positionSlider(p);
+    setPercentage(tMarket, pMarket, p);
+    setPercentage(tUser, pUser, p);
 }
 
 function initialize() {
