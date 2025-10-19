95da45ba0f9260484a04586ad7829e883b03d800
Ruud van Asseldonk <ruud@chorus.one>
2025-10-19 23:59:41 +0200

    Be a bit more positive, rephrase help text a bit

--- src/routes/help.rs
+++ src/routes/help.rs
@@ -27,21 +27,25 @@ fn view_help(ctx: &Context) -> Markup {
                         This manual provides a short introduction to prediction markets,
                         and how they work in Predict-o-matic."
                     }
-                    h2 { "Brief overview" }
+                    p { "This manual is a work in progress." }
+                    h2 { "Introduction" }
                     p {
-                        "The manual is not finished, but the very short version:
-                        You have points. You have "
+                        "You have points. You have "
                         (format!("{:.2}", ctx.user_points))
                         " of them right now.
                         You can also see this in the top-right corner of the screen.
                         You can deposit points into " em { "markets" } ".
                         When you do that, you purchase " em { "outcome shares" }
-                        " in that market. For example, you deposit $\u{200a}10,
+                        " in that market. For example, you deposit $\u{200a}10
+                        into a market “Will it rain next Tuesday?”,
                         and you receive 10 Yes and 10 No shares in return.
-                        When the market resolves, the shares for the outcome it
-                        resolved to, pay out $1. The other shares become worthless.
-                        This means that 1 Yes + 1 No is always worth $1.
-                        "
+                        When the market resolves,
+                        if it did rain, the Yes shares pay out $1 each,
+                        and the No shares become worthless.
+                        If it did not rain, the No shares pay out $1,
+                        and the Yes shares are worthless.
+                        Because the two outcomes are the only possibilities,
+                        1 Yes + 1 No is always worth $1."
                     }
                     p {
                         "Now that you have some outcome shares, you can trade them.
@@ -49,11 +53,11 @@ fn view_help(ctx: &Context) -> Markup {
                         not by trading them for points directly.
                         For example, you buy 5 Yes shares,
                         and you pay for that with 10 No shares.
-                        The ratio of Yes:No corresponds to the " em { "odds" }
-                        " of a negative outcome.
-                        Odds of 5:10 mean a probability of 5/15,
-                        so about 33% probability that ‘No’ will happen,
-                        and 67% that ‘Yes’ will happen.
+                        The ratio of No:Yes corresponds to the " em { "odds" }
+                        " of a positive outcome.
+                        Odds of 10:5 mean a probability of 10/15,
+                        so about 67% probability that ‘Yes’ will happen,
+                        and 33% that ‘No’ will happen.
                         In other words, the average price you paid was $0.67 per
                         Yes share."
                     }
@@ -62,10 +66,10 @@ fn view_help(ctx: &Context) -> Markup {
                         market maker that is managed by the system. The market
                         maker ensures that you can always trade, and it provides
                         a way to subsidize the market. Prediction markets are
-                        zero-sum, if you are going to make money here, "
+                        zero-sum. If you are going to make money here, "
                         em { "somebody" } " has to be losing money. When you are
-                        the only participant in a market (or when all
-                        participants bet in the same direction), it’s the system
+                        the only participant in a market, or when all
+                        participants bet in the same direction, it’s the system
                         that takes the other side of the bet."
                     }
                 }
